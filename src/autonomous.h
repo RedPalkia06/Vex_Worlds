e193a1bde5bee3a7bc1554516a8c0802995081d2
Alex Sloat <aisloat05@gmail.com>
2024-04-17 15:03:54 -0400

    added autos to autonomous.cpp

--- src/autonomous.h
+++ src/autonomous.h
@@ -1,6 +1,13 @@
 #ifndef AUTONOMOUS_H
 #define AUTONOMOUS_H
+#include "vex.h"
+#include "drivetrain.h"
+#include "constants.h"
+#include "math_utils.h"
+#include "drivercontrol.h"
+#include "pneumatic_wings.h"
+#include <iostream>
 
-void six_piece_auto();
+void six_piece_auto(Drivetrain drivetrain, PneumaticWing side_wing, PneumaticWing horizontal_wing, motor intake);
 
 #endif
